36e28c8c221732d821ecf445fdc58c7b1af91b56
Honcharov Yevhen <40567209+EvGonCode@users.noreply.github.com>
2023-05-25 23:49:05 +0300

    Add files via upload

---
+++ Laba_OP_6_23/Laba_OP_6_23/Laba_OP_6_23.cpp
@@ -0,0 +1,28 @@
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+	string input;
+	string type;
+	string mainType;
+	CircularLinkedList<string> sList;		//
+	CircularLinkedList<int> iList;			// used based on users choise
+	CircularLinkedList<float> fList;		//
+	try {
+
+		initiateInputLists(input, type, mainType, sList, iList, fList);
+
+		cout << "Generated list: " << endl;
+		printOneList(mainType, sList, iList, fList);
+
+		testDeletingElements(mainType, sList, iList, fList);
+
+
+		cout << "Deleting list...\n";
+		testDeletingList(mainType, sList, iList, fList);
+	}
+	catch (exception e) {
+		cout << e.what() << endl;
+	}
+}
